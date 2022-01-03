e87b1486e09ff94194c4a29480f40aed670798f0
Dave Amiana <amianism25@gmail.com>
2022-01-03 20:47:09 +0800

    updated savefile

--- src/BoardGenerator.py
+++ src/BoardGenerator.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from typing import List, Tuple, Union, Optional
+from typing import List, Tuple, Union
+import os
 
 class BoardMatrix:
     """Representation of the Board Matrix for the Chinese Checkers.
@@ -20,12 +21,8 @@ class BoardMatrix:
     
     def __str__(self) -> str: 
         str_lst = [list(map(lambda x: ' ' if x is None else x, y)) for y in self.mat]
-        return ''.join(''.join(f'| {x} |' for x in y) + '\n' for y in str_lst)
+        return '\n'.join('|'.join(f' {x} ' for x in y) + '\n' + (self.max -1)*'---+' for y in str_lst)
         
-    def __repr__(self) -> str: 
-        str_lst = [list(map(lambda x: ' ' if x is None else x, y)) for y in self.mat]
-        return ''.join(''.join(f'| {x} |' for x in y) + '\n' for y in str_lst)
-
     def __getitem__(self, index:Tuple[int, int]): 
         r, c = index
         if r < self.row or c < self.col:
@@ -62,22 +59,46 @@ class BoardGenerator:
         if not (row, col) in ((7,6),(8,7)):
             raise ValueError('Incorrect input')
         
-        self.limit = 5 if row == 8 else 4
+        self.limit = 5 if (row,col) == (8,7) else 4
+        self.__board = BoardMatrix(row, col)
         self.row, self.col = row, col
     
     def __str__(self) -> str: 
-        ...
-    def __repr__(self) -> str: ...
+        return self.__board.__str__()
 
-    def save_game(self) -> None: ...
-    def update(self, row:int, col:int) -> None: ...
-    def get_score(self) -> Tuple[int, int]: ...
+    def save_game(self, fname:str) -> None:
+        dirname = os.path.dirname(__file__)
+        fname_path = os.path.join(dirname, f'{fname}.txt')
+        with open(fname_path, 'a') as f:
+            f.write(str(self))
+
+    def __getitem__(self, index: Tuple[int, int]):
+        r, c = index
+        if r < self.row or c < self.col:
+            return self.__board[r,c]
+        raise IndexError('invalid index')
+
+    def __setitem__(self, key: Tuple[int, int], value):
+        r, c = key
+        self.__board[r,c] = value
+
+    def __update_p1(self) -> None: 
+        self.p1 += 1 
+    
+    def __update_p2(self) -> None: 
+        self.p2 += 1
+    
+    def get_score(self) -> Tuple[int, int]: 
+        return self.p1, self.p2
 
 
 if __name__ == '__main__':
-    # bg = BoardGenerator(6,8)
-    bm = BoardMatrix(4,5)
-    bm[1,2] = 'x'
-    bm[1,3] = 'o'
-    bm[2,2] = 'x'
-    print(bm)
+    bg = BoardGenerator(8,7)
+    # bg = BoardMatrix(4,5)
+    bg[0,0]='x'
+    bg[3,4]='o'
+    bg[1,2]='x'
+    bg[1,3]='o'
+    bg[2,2]='x'
+    print(bg)
+    bg.save_game('game')
